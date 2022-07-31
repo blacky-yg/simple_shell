28976918af219a49e93862846565c2098ae894de
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-07-31 02:04:28 +0100

    init: setup repository structure

---
+++ shell.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * main - main block
+ * @ac: number of arguments
+ * @av: array containing the arguments
+ *
+ * Return: 0
+ */
+int main(__attribute__((__unused)) int ac,
+		__attribute__((__unused)) char *av[])
+{
+	return (0);
+}
