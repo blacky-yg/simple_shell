467b3a15bc1c7fdfb4649d8e01d92951a501d56c
GodblessMe2 <oshindeinde@gmail.com>
2022-08-10 23:24:12 +0000

    fix correction

--- str_funcs1.c
+++ str_funcs1.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 int _strlen(const char *s);
-char *_strcpy(char *dest, const char *src);
+char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, const char *src, size_t n);
 
